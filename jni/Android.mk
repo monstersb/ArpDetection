5031e2993786b03f8185348152e9342ff39683dd
Monster <naocanmonster@gmail.com>
2014-11-19 10:22:29 +0800

    create project

---
+++ jni/Android.mk
@@ -0,0 +1,6 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ArpDetection
+LOCAL_SRC_FILES := monster.c
+include $(BUILD_EXECUTABLE)
---
+++ obj/local/armeabi/objs/ArpDetection/monster.o.d
@@ -0,0 +1,52 @@
+obj/local/armeabi/objs/ArpDetection/monster.o: jni/monster.c \
+ /Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/stdio.h \
+ /Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ /Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ /Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ /Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/stdint.h \
+ /Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ /Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ /Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h \
+ /Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ /Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ /Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ /Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ /Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ /Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ /Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ /Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h
+
+/Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/stdio.h:
+
+/Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+/Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+/Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+/Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+/Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+/Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+/Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+/Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+/Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+/Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+/Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+/Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+/Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+/Applications/eclipse/android-ndk-r10/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
